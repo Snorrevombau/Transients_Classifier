5fbb2a7503eb92221e591176fdbbc838c8e87b43
markusodenthal <markus.odenthal@outlook.de>
2017-08-03 16:42:46 +0200

    fix bugs

--- SEPERATE_DF.py
+++ SEPERATE_DF.py
@@ -1,13 +1,20 @@
 import pandas as pd
 from collections import Counter
-# documentation : https://docs.python.org/2/library/collections.html
 
 
 def seperate_transients(input_df):
+    # get a df with all car events plugin or unplug
+    data_ev_charge_event = input_df[(input_df['P_Plugin'] == 1) |
+                                    (input_df['P_Unplug'] == 1)]
+    # convert the data_ev_charge_events indexes to a list
+    ev_charge_indexes = data_ev_charge_event.index.tolist()
+    # seperate the hole raw data, into events from outside and ev events
+    events_from_outside = input_df[~(input_df.index.isin(ev_charge_indexes))]
+    ev_events = input_df[(input_df.index.isin(ev_charge_indexes))]
     # put all timestamps to a list
     # and create a dict with Counter() to get a timestamp as key and a
     # value how often we can find this timestamp
-    timestamps = input_df.index.tolist()
+    timestamps = ev_events.index.tolist()
     # create a counter object
     counter_timestamps = Counter()
     # count how often one Timestamp is in the list
@@ -19,31 +26,21 @@ def seperate_transients(input_df):
     # create three new df
     # take the header from the input data
     header = input_df.dtypes.index
-    events_from_outside = pd.DataFrame(columns=header)
     one_phase_df = pd.DataFrame(columns=header)
     two_phase_df = pd.DataFrame(columns=header)
     three_phase_df = pd.DataFrame(columns=header)
-    for index, row in input_df.iterrows():
-        # ev_start 1 mean that ev start charging
-        ev_start = row['P_Plugin']
-        # ev_stop 1 mean that ev stop charging
-        ev_stop = row['P_Unplug']
-        # seperate events from outside and charging events
-        if(ev_start == 0 & ev_stop == 0):
-            # events from outside
-            events_from_outside = events_from_outside.append(row)
-        else:
-            # charging events
-            num_phase = counter_timestamps_series[index]
-            # Events with one phase
-            if(num_phase == 1):
-                one_phase_df = one_phase_df.append(row)
-            # Events with two phases
-            elif(num_phase == 2):
-                two_phase_df = two_phase_df.append(row)
-            # Events with three phases
-            elif(num_phase == 3):
-                three_phase_df = three_phase_df.append(row)
+    for index, row in ev_events.iterrows():
+        # charging events
+        num_phase = counter_timestamps_series[index]
+        # Events with one phase
+        if(num_phase == 1):
+            one_phase_df = one_phase_df.append(row)
+        # Events with two phases
+        elif(num_phase == 2):
+            two_phase_df = two_phase_df.append(row)
+        # Events with three phases
+        elif(num_phase == 3):
+            three_phase_df = three_phase_df.append(row)
     # take the dtypes from the input header
     dtypes = dict(input_df.dtypes)
     events_from_outside = events_from_outside.astype(dtype=dtypes)
